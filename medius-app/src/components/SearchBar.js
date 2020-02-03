7ddc982e0208d5d21eba07ebd4323baf56a3d023
Bradley Ramos <bradleymramos@gmail.com>
2020-02-03 00:45:06 -0600

    search by feature

--- medius-app/src/components/SearchBar.js
+++ medius-app/src/components/SearchBar.js
@@ -1,19 +1,53 @@
 import React, { useState,useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import firebase from '../Firebase';
 
+const useStyles = makeStyles({
+  option: {
+    fontSize: 15,
+    '& > span': {
+      marginRight: 10,
+      fontSize: 18,
+    },
+  },
+});
+
+function rateCheck(score) {
+  let color
+  if (score > 4.5) {
+    color = 'green'
+  } else if (score > 3.0) {
+    color = 'orange'
+  } else {
+    color = 'red'
+  }
+  return color;
+}
+
 const SearchBox = ({data}) => {
   console.log("SearchBox data", data)
+  const classes = useStyles();
   return(
   <Autocomplete style={{
-    position: 'absolute', left: '50%', top: '50%', width: 300,
+    position: 'absolute', left: '50%', top: '50%', width: 400,
     transform: 'translate(-50%, -50%)'}}
     id="combo-box-demo"
     options={Object.keys(data).length === 0?[]:data}
+    classes={{
+      option: classes.option,
+    }}
     getOptionLabel={option => option.name}
+    renderOption={option => (
+      <React.Fragment>
+        <span style={{color: rateCheck(option.rating)}}>{option.rating}</span>
+        {option.name} 
+        <span style={{color: 'grey', fontSize: 12}}> ({option.profession}) </span>
+      </React.Fragment>
+    )}
     renderInput={params => (
-    <TextField {...params} label="Select your contractor!" variant="outlined" fullWidth />
+    <TextField {...params} label="Search by name, rating, or profession" variant="outlined" fullWidth />
   )}
 />)
 };
